316c6118d8874d0eeae192dd552cd79b6a5f54a3
Tim Hanlon <tim@timhanlon.com>
2025-01-13 17:31:53 +1100

    cleanup playgrounds

--- playground-custom/pages/index.vue
+++ playground-custom/pages/index.vue
@@ -1,40 +1,13 @@
 <template>
-  <div class="container">
-    <h1>Bedtime Playground</h1>
-    <p>Welcome to the Bedtime playground! This demo includes:</p>
-    <ul>
-      <li>
-        <NuxtLink to="/stories">Default story viewer</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/custom">Custom story viewer</NuxtLink>
-      </li>
-    </ul>
+  <div class="h-screen flex items-center justify-center flex-col gap-4">
+    <h1 class="text-xl font-medium tracking-tight">
+      Bedtime Playground
+    </h1>
+    <UButton
+      to="/custom"
+      trailing-icon="i-heroicons-arrow-right"
+    >
+      Custom Story Viewer
+    </UButton>
   </div>
 </template>
-
-<style scoped>
-.container {
-  max-width: 800px;
-  margin: 2rem auto;
-  padding: 0 1rem;
-}
-
-h1 {
-  font-size: 2rem;
-  margin-bottom: 1rem;
-}
-
-ul {
-  margin-top: 1rem;
-}
-
-a {
-  color: #00dc82;
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-</style>
