ff6057d532fe811367e982c7e9708dde850a8e06
Tim Hanlon <tim@timhanlon.com>
2025-01-21 17:26:53 +1100

    feat: command palette

---
+++ playground/components/CommandPalette.story.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const items = ref([
+  { id: 1, label: 'Item 1', action: () => { console.log('Item 1') } },
+  { id: 2, label: 'Item 2', action: () => { console.log('Item 2') } },
+])
+
+const { stories } = useStory()
+
+const storyItems = computed(() => {
+  return Object.values(stories).map(story => ({
+    id: story.kebabName,
+    label: story.pascalName,
+    action: () => { navigateTo(`/stories/${story.kebabName}`) },
+  }))
+})
+</script>
+
+<template>
+  <Story title="Command Palette">
+    <div class="flex items-center gap-1">
+      <Key symbol="meta" />
+      <Key symbol="J" />
+    </div>
+    <CommandPalette
+      :items="storyItems"
+      key-combo="meta_j"
+    />
+  </Story>
+</template>
