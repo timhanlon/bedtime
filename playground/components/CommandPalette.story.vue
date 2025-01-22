1648320f2c81468cd76b9bf497d40ce561a5f265
Tim Hanlon <tim@timhanlon.com>
2025-01-22 12:23:31 +1100

    chore: remove old items

--- playground/components/CommandPalette.story.vue
+++ playground/components/CommandPalette.story.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-const items = ref([
-  { id: 1, label: 'Item 1', action: () => { console.log('Item 1') } },
-  { id: 2, label: 'Item 2', action: () => { console.log('Item 2') } },
-])
-
 const { stories } = useStory()
 
 const storyItems = computed(() => {
