3029fa0469ab7c2a02aee9fc5fd7cebc21ceeb1b
timhanlon <tim@timhanlon.com>
2025-02-03 11:49:29 +1100

    perf: remove lucide-vue-nuxt dependency (#15)
    * perf: remove lucide-vue-nuxt dependency
    
    * chore: ci
    
    * chore: ci
    
    * chore: ci
    
    * chore: ci

--- src/runtime/components/CommandPalette.vue
+++ src/runtime/components/CommandPalette.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, computed, watch, nextTick } from 'vue'
-import { Search } from 'lucide-vue-next'
 import { onKeyStroke, useMagicKeys, onClickOutside } from '@vueuse/core'
 
 interface CommandItem {
@@ -122,10 +121,25 @@ function scrollIntoView(index: number) {
           class="command-palette-input"
           @focus="isOpen = true"
         >
-        <Search
-          class="command-palette-icon"
-          :size="18"
-        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-search command-palette-icon"
+        >
+          <circle
+            cx="11"
+            cy="11"
+            r="8"
+          />
+          <path d="m21 21-4.3-4.3" />
+        </svg>
       </div>
       <div
         ref="itemsRef"
